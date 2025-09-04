3abf0a2cee2436a3a9efea1ce698fb5b2b3751f4
Mathias Stadler <stadler-mathias@web.de>
2025-09-04 17:56:40 +0200

    save

--- examples/save_plot/src/main.rs
+++ examples/save_plot/src/main.rs
@@ -12,7 +12,7 @@ fn main() -> eframe::Result {
         ..Default::default()
     };
     eframe::run_native(
-        "My egui App with a plot",
+        "Change here My egui App with a plot",
         options,
         Box::new(|_cc| Ok(Box::<MyApp>::default())),
     )
